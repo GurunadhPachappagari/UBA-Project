3005b88805bdb87e39b1279037a2a189bfd03bb8
GurunadhPachappagari <gurunadh.pg123@gmail.com>
2022-02-09 22:48:10 +0530

    done selection

--- SurveyStats/js/script.js
+++ SurveyStats/js/script.js
@@ -4,34 +4,8 @@ import * as Data from './select_data.js'
 import * as Helpers from './Helpers.js'
 import * as WProcess from './WordProcessing.js'
 
-var data = Data.data
+var data = Data.data;
 
-
-for(var i = 0; i < data.length; i++){
-    var file = data[i];
-    var json_data = await Helpers.getData(file.file_path);
-    for(var j = 0; j < file.cols.length; j++){
-        var column = file.cols[j];
-        var column_name = column.name;
-        var description = column.description;
-        if(WProcess.isSensetive(column_name)){
-            continue;
-        }
-        
-        var canvas = document.createElement('canvas');
-        var id = file.file_path + '/' + column_name;
-        canvas.setAttribute('id', id);
-        document.getElementById('plots').appendChild(canvas);
-
-        if(column.plot_type == 'pie'){
-            PieChart.drawPie(file.file_path, column_name, json_data, description);
-        }
-        else if(column.plot_type == 'histogram'){
-            var bins = column.bins;
-            Histogram.drawHist(file.file_path, column_name, bins, json_data, description)
-        }
-    }
-}
 
 
 // window.onload = function () {
@@ -52,8 +26,46 @@ for(var i = 0; i < data.length; i++){
         for(var j = 0; j < data[this.value].cols.length; j++){
             var column = data[this.value].cols[j].name;
             var newel = document.createElement('td');
-            newel.innerHTML = "<input type='checkbox' id=" + column + ">" + column;
+            newel.innerHTML = "<input type='checkbox' id=" + column.replace(/ /g, '_') + ">" + column;
             row.appendChild(newel);
+            // console.log(column);
         }
     }
 // }
+
+
+var form = document.getElementById("dSetFormSubmit")
+form.addEventListener("click", async function(evt){
+    document.getElementById('plots').innerHTML = ""
+    for(var i = 0; i < data.length; i++){
+        var file = data[i];
+        var json_data = await Helpers.getData(file.file_path);
+        for(var j = 0; j < file.cols.length; j++){
+            var column = file.cols[j];
+            var column_name = column.name;
+            var description = column.description;
+            var check_box = document.getElementById(column_name.replace(/ /g, '_'));
+            if( check_box == null || check_box.checked == false){
+                // console.log("not selected " + column_name)
+                continue;
+            }
+            if(WProcess.isSensetive(column_name)){
+                continue;
+            }
+            
+            var canvas = document.createElement('canvas');
+            var id = file.file_path + '/' + column_name;
+            canvas.setAttribute('id', id);
+            document.getElementById('plots').appendChild(canvas);
+
+            if(column.plot_type == 'pie'){
+                PieChart.drawPie(file.file_path, column_name, json_data, description);
+            }
+            else if(column.plot_type == 'histogram'){
+                var bins = column.bins;
+                Histogram.drawHist(file.file_path, column_name, bins, json_data, description)
+            }
+        }
+    }
+
+})
